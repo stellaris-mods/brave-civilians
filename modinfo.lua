af7383178c299a113bb718f620a0d51ae6cece4c
Folk <vhaarr@gmail.com>
2017-10-12 23:16:26 +0200

    Update for 1.8.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Brave Civilians (civilian combat computers)",
 	tags = { "Gameplay", "Fixes" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 816862071,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
